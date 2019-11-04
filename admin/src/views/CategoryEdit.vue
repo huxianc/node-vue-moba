19cb84be0afe7d5afe37fd7930c51d9e3220d5b8
dulihua <928965406@qq.com>
2019-11-04 17:10:26 +0800

    11/4 init

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>新建分类</h1>
+    <el-form :model="model" ref="model" label-width="80px" @submit.native.prevent="save">
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      model: {
+        name: ""
+      }
+    };
+  },
+  methods: {
+    async save() {
+      const res = await this.$http.post("categories", this.model);
+      this.$router.push("/categories/list");
+      this.$message.success("保存成功"+res)
+    }
+  }
+};
+</script>
---
+++ admin/src/views/CategoryList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>分类列表</h1>
+    <el-table :data="items" border stripe>
+      <el-table-column prop="_id" label="ID" width="220"></el-table-column>
+      <el-table-column prop="name" label="分类名称"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created() {
+    this.fetch();
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get("categories");
+      this.items = res.data;
+    }
+  }
+};
+</script>
